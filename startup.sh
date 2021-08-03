fb4a31935f37452c12b7ed31b3030ad5b854e1f6
Aniket Gadre <aniket.gadre@grabtaxi.com>
2021-08-03 14:32:58 +0800

    update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,10 @@
-if [[ -f "/vault/secrets/dev-treco-db01-rw" ]]
+
+db_creds_file="/vault/secrets/treco_db_creds"
+if [[ -f $db_creds_file ]]
 then
-    source "/vault/secrets/dev-treco-db01-rw"
+    source $db_creds_file
+else
+    echo "$db_creds_file doesn't exists"
 fi
 
 ./treco serve
